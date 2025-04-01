d7cdca22bdd01f9e5b00e8a427663ba9ba4b3165
keithadavidsonACE <138878702+keithadavidsonACE@users.noreply.github.com>
2025-04-01 21:43:31 +0100

    Initial Payload commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
+const { withPayload } = require("@payloadcms/next/withPayload");
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPayload(nextConfig)
